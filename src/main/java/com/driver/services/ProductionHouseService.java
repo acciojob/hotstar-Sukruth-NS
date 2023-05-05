93c5c6ad5196c966d3c3daaa06ee4dbfb1380183
Sukruth Surendra N S <Sukruth.sns@campusuvce.in>
2023-05-05 20:08:46 +0530

    Soln

--- src/main/java/com/driver/services/ProductionHouseService.java
+++ src/main/java/com/driver/services/ProductionHouseService.java
@@ -15,9 +15,13 @@ public class ProductionHouseService {
 
     public Integer addProductionHouseToDb(ProductionHouseEntryDto productionHouseEntryDto){
 
-        return  null;
+        ProductionHouse productionHouse = new ProductionHouse(productionHouseEntryDto.getName());
+
+        productionHouse.setRatings(0.0);
+
+        ProductionHouse addedProductionHouse = productionHouseRepository.save(productionHouse);
+
+
+        return addedProductionHouse.getId();
     }
-
-
-
 }
--- src/main/java/com/driver/services/UserService.java
+++ src/main/java/com/driver/services/UserService.java
@@ -23,9 +23,9 @@ public class UserService {
 
 
     public Integer addUser(User user){
-
         //Jut simply add the user to the Db and return the userId returned by the repository
-        return null;
+        User user1 = userRepository.save(user);
+        return user1.getId();
     }
 
     public Integer getAvailableCountOfWebSeriesViewable(Integer userId){
@@ -33,9 +33,34 @@ public class UserService {
         //Return the count of all webSeries that a user can watch based on his ageLimit and subscriptionType
         //Hint: Take out all the Webseries from the WebRepository
 
+        User user = userRepository.findById(userId).get();
 
-        return null;
+        Subscription subscription = user.getSubscription();
+
+        SubscriptionType subscriptionType = subscription.getSubscriptionType();
+
+        List<WebSeries> webSeriesList = webSeriesRepository.findAll();
+
+        int count=0;
+
+        if(subscriptionType.toString().equals("BASIC")){
+            for(WebSeries web : webSeriesList){
+                if(web.getSubscriptionType().toString().equals("BASIC"))
+                    count++;
+            }
+        }
+        else if (subscriptionType.toString().equals("PRO")) {
+            for(WebSeries web : webSeriesList){
+                if(web.getSubscriptionType().toString().equals("PRO") || web.getSubscriptionType().toString().equals("BASIC") )
+                    count++;
+            }
+        }
+        else {
+            for(WebSeries web : webSeriesList){
+                count++;
+            }
+        }
+
+        return count;
     }
-
-
 }
